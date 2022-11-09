944b5def2bfcb685d53099b9f2ae5f91876b8a11
Svetlana Chikalova <chikalova.sveta@mail.ru>
2022-11-09 13:20:24 +0700

    Добавлены тесты

--- main.cpp
+++ main.cpp
@@ -7,15 +7,21 @@
 using namespace std;
 
 int main()
-{
-       FileMonitor monitor("monitor.txt");
-       FileObserver observer;
-           monitor.Attach(&observer);
+{           // объект для отслеживания файла
+            FileMonitor monitor("C:\\CHIKAEED\\untitled6\\monitor.txt");
+            FileObserver observer;
 
-        while (true){
-                   monitor.checkFile();
-                   this_thread::sleep_for(chrono::milliseconds(1000));
-               }
+            monitor.Attach(&observer);
+
+                int iter = 1;
+                // бесконечный цикл для отслеживания состояния файла
+                while (true){
+                    cout << "current iteration: " << iter << endl;
+                    iter++;
+                    monitor.checkFile();
+                    this_thread::sleep_for( chrono::milliseconds( 1000 ) );
+                }
+
 
             return 0;
 }
